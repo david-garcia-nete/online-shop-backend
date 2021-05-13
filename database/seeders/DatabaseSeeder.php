bed89bd78a9098817a6d23356db1e72ff0e51579
David Garcia <david.garcia.nete@gmail.com>
2021-05-13 14:13:57 -0400

    Fix database seeder and start creating JWT authentication toke.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-      $this->call('UsersTableSeeder');
-      $this->call('PaymentMethodsTableSeeder');
+      $this->call([
+        UsersTableSeeder::class,
+        PaymentMethodsTableSeeder::class,
+    ]);
     }
 }
